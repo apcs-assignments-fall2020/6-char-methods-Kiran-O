c9028250c06554287d6e61f31fa3d2a4496d7dd7
Kiran-O <22kokelly@athenian.org>
2020-09-21 04:28:51 +0000

    Passed all tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -6,35 +6,80 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (65 <= ch && ch <= 90){
+            ch = (char) ((ch)+32);
+            return(ch);
+        
+        }
+        else {
+            ch = (ch);
+            return(ch);
+        }
+        
     }
 
     // Returns a copy of a String, where all letters 
     // are now lowercase
     public static String myToLowerCase(String str) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        String ert = "";
+        for (int k = 0; k < str.length(); k++){
+            char wer = str.charAt(k);
+            if (65 <= wer && wer <= 90){
+                wer = (char) ((wer)+32);
+            }
+            ert = ert + wer;
+            
+        }
+        return(ert);
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not an lowercase letter, 
     // it is returned unchanged. 
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (97 <= ch && ch <= 122){
+            ch = (char) ((ch)-32);
+            return (ch);
+        }
+        else {
+            ch = (ch);
+            return (ch);
+        }
+        
     }
 
     // Returns a copy of a String, where all letters 
     // are now uppercase
     public static String myToUpperCase(String str) {
-        // REPLACE WITH YOUR CODE HERE
-        return "";
+        String qaw = "";
+        for (int k = 0; k < str.length(); k++){
+            char wsx = str.charAt(k);
+            if (97 <= wsx && wsx <= 122){
+                wsx = (char) ((wsx)-32);
+            }
+            qaw = qaw + wsx;
+        }
+        return(qaw);
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a string");
+        String str = scan.next();
+        System.out.println("Do you want your string uppercase or lowercase?");
+        String lwer = "lowercase";
+        String upr = "uppercase";
+        String grb = scan.next();
+        if (grb == lwer){
+            System.out.println("Here is your string in "+ lwer + myToLowerCase(str));
+        }
+        if (grb == upr){
+            System.out.println("Here is your string in "+ upr + myToUpperCase(str));
+        }
+        else {
+            System.out.println("Please make sure you made no spelling errors");
+        }
+
     }
 }
